2f63f15e3d52016fc27a1052381fb5018486720d
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-11-10 15:39:19 +0530

    goals are rendering

---
+++ src/Pages/GoalPage.jsx
@@ -0,0 +1,69 @@
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchGoalsAsync, deleteGoalAsync } from "../Features/goalSlice";
+import { MdDelete } from "react-icons/md";
+
+function GoalList(props) {
+  const taskId = props.taskId;
+  console.log(taskId)
+  const dispatch = useDispatch();
+  const goals = useSelector((state) => state.goal.data);
+  const status = useSelector((state) => state.goal.status);
+  const error = useSelector((state) => state.goal.error);
+
+  const handleGoalDelete = (goalId) => {
+    dispatch(deleteGoalAsync(goalId)); // Assuming deletegoal action handles API call
+  };
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchGoalsAsync());
+    }
+  }, [status, dispatch]);
+
+  if (status === "loading") {
+    return <div>Loading...</div>;
+  }
+
+  if (status === "failed") {
+    return <div>Error: {error}</div>;
+  }
+
+  return (
+    <div>
+      <div>
+        <ul className="flex flex-col">
+          {goals.map((goal) => {
+            return taskId === goal.task_id ? (
+              <div
+                key={goal.id}
+                className="w-[280px] bg-white  dark:bg-[#2b2c37] shadow-[#364e7e1a] rounded-lg mx-auto my-4 py-6 px-3 shadow-lg cursor-pointer"
+              >
+                <li>
+                  <div className="flex items-center justify-between">
+                    <div className="text-black text-xl dark:text-white font-bold">
+                      {goal.title}
+                    </div>
+                    <div>
+                      <MdDelete
+                        className="cursor-pointer"
+                        onClick={() => handleGoalDelete(goal.id)}
+                      />
+                    </div>
+                  </div>
+                </li>
+              </div>
+            ) : (
+              <li key={goal.id} className="hidden"></li>
+            );
+          })}
+        </ul>
+      </div>
+      {/* <button onClick={} className="button">
+        Add Goal
+      </button> */}
+    </div>
+  );
+}
+
+export default GoalList;
--- src/Pages/TaskList.jsx
+++ src/Pages/TaskList.jsx
@@ -1,9 +1,73 @@
-import React from 'react'
+import React, { useEffect, useCallback } from "react";
+import GoalList from "./GoalPage";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchTasksAsync, deleteTaskAsync } from "../Features/taskSlice";
+import { MdDelete } from "react-icons/md";
 
-const TaskList = () => {
+function TaskList(props) {
+  const projectId = props.projectId;
+  const dispatch = useDispatch();
+  const projects = useSelector((state) => state.project.data);
+  const tasks = useSelector((state) => state.task.data);
+  const status = useSelector((state) => state.task.status);
+
+  const handleTaskDelete = useCallback(
+    async (taskId) => {
+      // Assuming your API call is handled in the 'deletetask' action
+      dispatch(deleteTaskAsync(taskId));
+    },
+    [dispatch]
+  );
+
+  useEffect(() => {
+    if (status === "idle") {
+      dispatch(fetchTasksAsync());
+    }
+  }, [status, dispatch]);
+
   return (
-    <div>TaskList</div>
-  )
+    <div className="flex gap-3">
+      <div>
+        <div>
+          <ul className="flex flex-row gap-3">
+            {tasks.map((task) => {
+              return task.project === projectId ? (
+                <div key={task.id}>
+                  <li className="w-[300px] dark:text-white px-3 py-3 dark:bg-[#20212c] gap-3">
+                    <p className="font-bold text-center uppercase flex items-center justify-around">
+                      {task.Task_name}
+                      <MdDelete
+                        className="cursor-pointer"
+                        onClick={(e) => {
+                          e.stopPropagation();
+                          handleTaskDelete(task.id);
+                        }}
+                      />
+                    </p>
+                  </li>
+                  <div className="mt-2">
+                    <GoalList taskId={task.id} />
+                  </div>
+                </div>
+              ) : (
+                <li key={task.id} className="hidden"></li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+      <div
+        className={
+          projects.length === 0
+            ? "hidden"
+            : 'className=" h-[100vh] flex justify-center items-center font-bold text-2xl hover:text-[#635FC7] transition duration-300 cursor-pointer bg-gray-200  dark:bg-[#20212c]  scrollbar-hide mb-2   mx-5 min-w-[280px] text-[#828FA3] rounded-lg"'
+        }
+        
+      >
+        Add a task
+      </div>
+    </div>
+  );
 }
 
-export default TaskList
+export default React.memo(TaskList);
