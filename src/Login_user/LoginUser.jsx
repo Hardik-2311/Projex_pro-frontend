9cbcc1b03796fc7488b064c79609f0d3f1ccadb5
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-11-13 23:12:42 +0530

    unsual rendereing of check function made stop

--- src/Login_user/LoginUser.jsx
+++ src/Login_user/LoginUser.jsx
@@ -1,23 +1,36 @@
 import axios from "axios";
 import { setUserData } from "../Features/singleUser";
 import { useDispatch } from "react-redux";
-axios.defaults.headers.post["Content-Type"]="multipart/form-data";
+import { useEffect } from "react";
+import toast from "react-hot-toast";
+
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.xsrfHeaderName = "X-CSRFTOKEN";
 axios.defaults.xsrfCookieName = "csrftoken";
 const axiosConfig = {
   withCredentials: true,
 };
-export default function CheckLogin(){
-    const dispatch =useDispatch()
-    axios.get("http://127.0.0.1:8000/check_login",axiosConfig).then((res)=>{
-        if(!(res.data.login)){
-             console.log("kuch to gadbad hai daya")
-            window.location.href='http://127.0.0.1:3000/';
-        }
-        else{
-            console.log("you can make this happen")
-            dispatch(setUserData(res.data.data))
-        }
-    })
-}
 
+export function useCheckLogin() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get("http://127.0.0.1:8000/check_login", axiosConfig);
+        if (!res.data.login) {
+        //   console.log("kuch to gadbad hai daya");
+          window.location.href = 'http://127.0.0.1:3000/';
+        } else {
+        //   console.log("you can make this happen");
+          dispatch(setUserData(res.data.data));
+        }
+      } catch (error) {
+        toast.error("login first")
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+  return null;
+}
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
-
+import React from "react";
+import { useEffect } from "react";
+import { fetchUsersAsync } from "../Features/userSlice";
+import { useSelector, useDispatch } from "react-redux";
 const Dashboard = () => {
-  return (
-    <div>Dashboard</div>
-  )
-}
+  const dispatch = useDispatch();
 
-export default Dashboard
+  const user = useSelector((state) => state.user.data);
+  console.log(user)
+  useEffect(() => {
+    dispatch(fetchUsersAsync());
+  }, [dispatch]);
+  return (
+    <div className="flex justify-center items-center">
+      <div className="text-3xl dark:text-white">
+        <h3>hello click on project to view details</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Dashboard;
