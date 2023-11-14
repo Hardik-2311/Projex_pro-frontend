2b78580536602738da5a3260857aeb1c68fbe6c2
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-11-14 17:38:05 +0530

    dashboard:

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,19 +1,28 @@
 import React from "react";
-// import { useEffect } from "react";
-// import { fetchUsersAsync } from "../Features/userSlice";
-// import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchUsersAsync } from "../Features/userSlice";
+import { useSelector, useDispatch } from "react-redux";
 const Dashboard = () => {
-  // const dispatch = useDispatch();
-
-  // const user = useSelector((state) => state.user.data);
-  // // console.log(user)
-  // useEffect(() => {
-  //   dispatch(fetchUsersAsync());
-  // }, [dispatch]);
+  const dispatch = useDispatch();
+  const projects = useSelector((state) => state.project.data);
+  const users = useSelector((state) => state.user.data);
+  // console.log(user)
+  useEffect(() => {
+    dispatch(fetchUsersAsync());
+  }, [dispatch]);
   return (
-    <div className="flex justify-center items-center">
+    <div className="flex flex-col justify-between space-y-10 items-center">
       <div className="text-3xl dark:text-white">
-        <h3>hello click on project to view details</h3>
+        {projects.length === 0 ? (
+          <h3>Create project</h3>
+        ) : (
+          <h3>hello click on project to view details</h3>
+        )}
+      </div>
+      <div className="text-3xl dark:text-white">
+        {users.map((user) => {
+          return <div>{user.name}</div>;
+        })}
       </div>
     </div>
   );
--- src/Pages/UserPage.jsx
+++ src/Pages/UserPage.jsx
@@ -1,37 +1,34 @@
 // UserPage.jsx
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { fetchUsersAsync } from '../Features/userSlice';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { fetchUsersAsync } from "../Features/userSlice";
 
 const UserPage = () => {
   const dispatch = useDispatch();
   const users = useSelector((state) => state.user.data);
-  const activeUsers =users.filter((user) => user.is_active===true)
-  activeUsers.map((activeuser)=>{
-    return activeuser
-  })
+  const activeUsers = users.filter((user) => user.is_active === true);
+  activeUsers.map((activeuser) => {
+    return activeuser;
+  });
   const status = useSelector((state) => state.user.status);
 
   useEffect(() => {
-    if (status === 'idle') {
-      dispatch(fetchUsersAsync());
-    }
-  }, [status, dispatch]);
+    dispatch(fetchUsersAsync());
+  }, [dispatch]);
 
   return (
     <div>
       <h1>User Profile</h1>
-      {status === 'loading' && <div>Loading...</div>}
-      {status === 'succeeded' && (
+      {status === "loading" && <div>Loading...</div>}
+      {status === "succeeded" && (
         <div>
-          <strong>Username:</strong> {
-            activeUsers.map((user)=>{
-                return <h1>{user.enrolment_no}</h1>
-            })
-          }
+          <strong>Username:</strong>{" "}
+          {activeUsers.map((user) => {
+            return <h1>{user.enrolment_no}</h1>;
+          })}
         </div>
       )}
-      {status === 'failed' && <div>Error loading user data</div>}
+      {status === "failed" && <div>Error loading user data</div>}
     </div>
   );
 };
