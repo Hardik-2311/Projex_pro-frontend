5825ae54ab5a7676fff0dff73e2ed37c9abed41c
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-11-14 17:47:29 +0530

    dashboard

--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,19 +1,30 @@
 import React from "react";
-// import { useEffect } from "react";
-// import { fetchUsersAsync } from "../Features/userSlice";
-// import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { fetchUsersAsync } from "../Features/userSlice";
+import { useSelector, useDispatch } from "react-redux";
 const Dashboard = () => {
-  // const dispatch = useDispatch();
-
-  // const user = useSelector((state) => state.user.data);
-  // // console.log(user)
-  // useEffect(() => {
-  //   dispatch(fetchUsersAsync());
-  // }, [dispatch]);
+  const dispatch = useDispatch();
+  const projects = useSelector((state) => state.project.data);
+  const users = useSelector((state) => state.user.data);
+  // console.log(user)
+  useEffect(() => {
+    dispatch(fetchUsersAsync());
+  }, [dispatch]);
   return (
-    <div className="flex justify-center items-center">
-      <div className="text-3xl dark:text-white">
-        <h3>hello click on project to view details</h3>
+    <div className="flex justify-center flex-col space-y-10 items-center">
+      <div className="text-2xl dark:text-white">
+        {projects.length === 0 ? (
+          <h3>Create project</h3>
+        ) : (
+          <h3>hello click on project to view details</h3>
+        )}
+      </div>
+      <div className="text-2xl dark:text-white flex flex-col">
+        {
+          users.map((user)=>{
+            return user.name
+          })
+        }
       </div>
     </div>
   );
