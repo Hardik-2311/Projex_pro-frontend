0d2bb356662aedc2d1c12837873600ab9f79df36
Hardik Narang <126074137+Hardik-2311@users.noreply.github.com>
2023-11-14 21:21:42 +0530

    login page

--- src/Pages/LoginPage.jsx
+++ src/Pages/LoginPage.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { BsFillSunFill, BsFillMoonFill } from "react-icons/bs";
 import { Switch } from "@headlessui/react";
 import useDarkMode from "../Hooks/useDark";
+import channel  from "../assets/channel.svg";
+import club from "../assets/club.png"
 const LoginPage = () => {
   const [colorTheme, setTheme] = useDarkMode();
   const [darkMode, setDarkMode] = useState(
@@ -16,25 +18,35 @@ const LoginPage = () => {
     window.location.href = "http://127.0.0.1:8000/login";
   };
 
-
   return (
     <div className={`w-full min-h-screen ${darkMode ? "dark" : ""}`}>
       <div
         className={`h-[100vh] flex justify-center items-center bg-slate-400 dark:bg-[#20212c]`}
       >
         <div className="flex flex-col items-center justify-center gap-5">
-          <div className="logo text-white dark:text-black">Projex_Pro</div>
-          <div>
-            <button
-              className="bg-purple-500 p-6 rounded-full"
-              onClick={handleClick}
-            >
-              Sign In with Channel I
-            </button>
+          <div className="rounded-md flex shadow-3xl py-10">
+            <div>
+              <div className="flex flex-col justify-center items-center">
+                <img src={club} width={64} height={64} alt="img"  />
+                <div className="flex flex-col items-start p-2 m-5">
+                  <div className="text-2xl font-bold  text-black dark:text-white">
+                    Welcome
+                  </div>
+                </div>
+                <button
+                  type="button"
+                  className="text-white bg-[#635fc7] hover:bg-[#402cff]/90 focus:ring-4 focus:outline-none focus:ring-[#3b5998]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex justify-between items-center dark:focus:ring-[#3b5998]/55 mr-2 mb-2 space-x-5"
+                  onClick={handleClick}
+                >
+                  <img src={channel} width={24} height={24} alt="channel i" />
+                  <div>Sign in with Channeli</div>
+                </button>
+              </div>
+            </div>
           </div>
           <div className="flex items-center text-white dark:text-black">
-            <div className="p-4 w-full justify-center relative space-x-2 bg-slate-100 dark:bg-[#20212c] flex  items-center rounded-lg">
-              <BsFillSunFill />
+            <div className="p-4 w-full justify-center relative space-x-2 bg-slate-500 dark:bg-[#20212c] flex  items-center rounded-lg">
+              <BsFillSunFill className="dark:text-white text-black" />
               <Switch
                 checked={darkMode}
                 onChange={toggleDarkMode}
@@ -45,14 +57,14 @@ const LoginPage = () => {
                 <span
                   className={`${
                     darkMode ? "translate-x-6" : "translate-x-1"
-                  } inline-block h-4 w-4 transform rounded-full bg-white transition`}
+                  } inline-block h-4 w-4 transform rounded-full bg-slate-500 transition`}
                 />
               </Switch>
-              <BsFillMoonFill />
+              <BsFillMoonFill className="dark:text-white text-black" />
             </div>
             <label className="switch">
               <input
-              hidden
+                hidden
                 type="checkbox"
                 onChange={toggleDarkMode}
                 checked={darkMode}
